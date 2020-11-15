99abf4cb460d8ed7ecde431071a7bda362433e8f
Philipp Stampfer <philipp.stampfer@protonmail.ch>
2020-11-15 15:46:14 +0100

    adjust combat system and enemy ui components

---
+++ Game/Assets/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isGamePaused;
+
+    void Start()
+    {
+        isGamePaused = true;
+        TogglePause();
+    }
+
+    public void RestartCurrentScene()
+    {
+        isGamePaused = false;
+        TogglePause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void TogglePause()
+    {
+        if (!isGamePaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        isGamePaused = true;
+    }
+
+    private void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isGamePaused = false;
+    }
+}
